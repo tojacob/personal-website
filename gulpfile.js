a4e8f576c1a635fee11770380621e4239f0582fa
Jacob Samuel G <26469677+tojacob@users.noreply.github.com>
2018-03-23 18:50:27 -0600

    config development environment

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+'use strict';
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const purgecss = require('gulp-purgecss')
+const purgeHtml = require('purgecss-from-html')
+
+// ===============
+// COMMON
+// ===============
+
+// Compile pug to html
+gulp.task('views', function buildHTML() {
+  return gulp.src('./src/views/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('./public/'));
+});
+
+// ===============
+// DEVELOPMENT
+// ===============
+
+// Compile .scss to css and add source map
+gulp.task('dev-styles', function () {
+  return gulp.src('./src/styles/app.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./public/styles'))
+    .pipe(browserSync.stream());
+});
+
+
+// Init local serve, live reald and call others dev tasks
+gulp.task('dev-serve', ['views', 'dev-styles'], function () {
+
+  browserSync.init({
+    server: "./public"
+  });
+
+  gulp.watch("./src/styles/*.scss", ['dev-styles']);
+  gulp.watch("./src/views/*.pug", ['views']).on('change', browserSync.reload);
+});
+
+// Run development tasks
+gulp.task('default', ['dev-serve']);
+
+// ===============
+// PRODUCTION
+// ===============
+
+// Compile scss to css, add prefixex and minify.
+gulp.task('prod-styles', function () {
+  return gulp.src('./src/styles/app.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([autoprefixer(), cssnano({ preset: 'default' })]))
+    .pipe(gulp.dest('./public/styles'))
+});
+
+// Remove unused css
+gulp.task('prod-purge-styles', ['prod-styles'], function () {
+  return gulp.src('./public/styles/*.css')
+    .pipe(
+      purgecss({
+        content: ['./public/*.html'],
+        extractors: [
+          {
+            extractor: purgeHtml,
+            extensions: ["html"]
+          }
+        ]
+      })
+    )
+    .pipe(gulp.dest('./public/styles'))
+})
+
+// Run production tasks
+gulp.task('prod', ['views', 'prod-purge-styles']);

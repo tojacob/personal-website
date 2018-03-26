1cf62ad32b5a8a139322027098424efc0dec0aa4
Jacob Samuel G <26469677+tojacob@users.noreply.github.com>
2018-03-26 01:03:11 -0600

    start dev

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,8 @@ const browserSync = require('browser-sync').create();
 const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
-const purgecss = require('gulp-purgecss')
-const purgeHtml = require('purgecss-from-html')
+const purgecss = require('gulp-purgecss');
+const purgeHtml = require('purgecss-from-html');
 
 // ===============
 // COMMON
@@ -16,7 +16,8 @@ const purgeHtml = require('purgecss-from-html')
 
 // Compile pug to html
 gulp.task('views', function buildHTML() {
-  return gulp.src('./src/views/*.pug')
+  return gulp
+    .src('./src/views/**/*.pug')
     .pipe(pug())
     .pipe(gulp.dest('./public/'));
 });
@@ -25,9 +26,10 @@ gulp.task('views', function buildHTML() {
 // DEVELOPMENT
 // ===============
 
-// Compile .scss to css and add source map
-gulp.task('dev-styles', function () {
-  return gulp.src('./src/styles/app.scss')
+// Compile scss to css and add source map
+gulp.task('dev-styles', function() {
+  return gulp
+    .src('./src/styles/app.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write())
@@ -35,49 +37,51 @@ gulp.task('dev-styles', function () {
     .pipe(browserSync.stream());
 });
 
-
-// Init local serve, live reald and call others dev tasks
-gulp.task('dev-serve', ['views', 'dev-styles'], function () {
-
+// Start local server, live reload and call others dev tasks
+gulp.task('dev-server', ['views', 'dev-styles'], function() {
   browserSync.init({
-    server: "./public"
+    server: './public'
   });
 
-  gulp.watch("./src/styles/*.scss", ['dev-styles']);
-  gulp.watch("./src/views/*.pug", ['views']).on('change', browserSync.reload);
+  gulp.watch('./src/styles/**/*.scss', ['dev-styles']);
+  gulp
+    .watch('./src/views/**/*.pug', ['views'])
+    .on('change', browserSync.reload);
 });
 
 // Run development tasks
-gulp.task('default', ['dev-serve']);
+gulp.task('default', ['dev-server']);
 
 // ===============
 // PRODUCTION
 // ===============
 
-// Compile scss to css, add prefixex and minify.
-gulp.task('prod-styles', function () {
-  return gulp.src('./src/styles/app.scss')
+// Compile scss to css, add prefixes and minify.
+gulp.task('prod-styles', function() {
+  return gulp
+    .src('./src/styles/app.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([autoprefixer(), cssnano({ preset: 'default' })]))
-    .pipe(gulp.dest('./public/styles'))
+    .pipe(gulp.dest('./public/styles'));
 });
 
 // Remove unused css
-gulp.task('prod-purge-styles', ['prod-styles'], function () {
-  return gulp.src('./public/styles/*.css')
+gulp.task('prod-purge-styles', ['prod-styles'], function() {
+  return gulp
+    .src('./public/styles/*.css')
     .pipe(
       purgecss({
-        content: ['./public/*.html'],
+        content: ['./public/**/*.html'],
         extractors: [
           {
             extractor: purgeHtml,
-            extensions: ["html"]
+            extensions: ['html']
           }
         ]
       })
     )
-    .pipe(gulp.dest('./public/styles'))
-})
+    .pipe(gulp.dest('./public/styles'));
+});
 
 // Run production tasks
 gulp.task('prod', ['views', 'prod-purge-styles']);

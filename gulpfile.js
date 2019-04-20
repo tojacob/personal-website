b65144f63013b712dadec9ed9f254c821bdb63ab
Jacob Samuel G <26469677+tojacob@users.noreply.github.com>
2019-04-19 19:00:45 -0500

    chore: refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 'use strict';
-const gulp = require('gulp');
+const { src, dest, series, parallel, watch } = require('gulp');
 const pug = require('gulp-pug');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
@@ -9,121 +9,119 @@ const tailwindcss = require('tailwindcss');
 const del = require('del');
 const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
-const purgecss = require('gulp-purgecss');
-const purgeHtml = require('purgecss-from-html');
 
 // ===============
 // COMMON
 // ===============
 
-// Folders
-const DIST = './docs',
-  DIST_STYLES = `${DIST}/assets/styles`,
-  DIST_ASSETS = `${DIST}/assets/`,
-  TO_DELETE = [`${DIST}/*.html`, `${DIST}/articles`, `${DIST}/assets`],
-  SRC_ASSETS = ['./src/assets/**/*.*'],
-  SRC_VIEWS = ['./src/views/pages/**/*.pug'],
-  SRC_STYLES = './src/styles/**/*.*',
-  MAIN_STYLE = './src/styles/app.scss',
-  TW_STYLE = './src/styles/tailwild/utilities.js';
+// Constantes
+const distFolder = './docs';
+const srcFolder = './src';
+const files = {
+  styles: {
+    main: `${srcFolder}/styles/app.scss`,
+    src: `${srcFolder}/styles/**/*.*`,
+    dist: `${distFolder}/assets/styles`,
+    tw: `${srcFolder}/styles/tailwild/utilities.js`
+  },
+  assets: {
+    src: `${srcFolder}/assets/**/*.*`,
+    dist: `${distFolder}/assets/`
+  },
+  views: {
+    src: [`${srcFolder}/views/pages/**/*.pug`],
+    dist: distFolder
+  }
+}
 
 // Clean dist folder before compile files
-gulp.task('clean-dist', function() {
-  return del(TO_DELETE);
-});
+function cleanDist() {
+  return del(distFolder);
+}
 
 // Move content from 'src/assets' to 'dist/assets' folder
-gulp.task('assets-to-dist', function() {
-  return gulp.src(SRC_ASSETS).pipe(gulp.dest(DIST_ASSETS));
-});
+function assetsToDist() {
+  return src(files.assets.src)
+    .pipe(dest(files.assets.dist));
+}
 
 // Compile pug to html
-gulp.task('views', function() {
-  return gulp
-    .src(SRC_VIEWS)
+function compileViews() {
+  return src(files.views.src)
     .pipe(pug())
-    .pipe(gulp.dest(DIST));
-});
+    .pipe(dest(distFolder));
+}
+
+function commonTasks() {
+  return series(
+    cleanDist,
+    assetsToDist,
+    compileViews
+  );
+}
 
 // ===============
 // DEVELOPMENT
 // ===============
 
 // Compile scss to css and add source map
-gulp.task('dev-styles', function() {
-  return gulp
-    .src(MAIN_STYLE)
+function devStyles() {
+  return src(files.styles.main)
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([tailwindcss(TW_STYLE)]))
+    .pipe(postcss([tailwindcss(files.styles.tw)]))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(DIST_STYLES))
+    .pipe(dest(files.styles.dist))
     .pipe(browserSync.stream());
-});
+}
 
 // Start local server, live reload and call others dev tasks
-gulp.task('dev-server', function() {
+function devServer(cb) {
   browserSync.init({
-    server: DIST
+    server: distFolder
   });
 
-  gulp.watch(SRC_STYLES, gulp.parallel('dev-styles'));
-  gulp
-    .watch(SRC_VIEWS, gulp.parallel('views'))
+  watch(files.styles.src, parallel(devStyles));
+  watch(files.views.src, parallel(compileViews))
     .on('change', browserSync.reload);
-});
+
+  return cb;
+}
 
 // Run development tasks
-gulp.task(
-  'dev',
-  gulp.series(
-    'clean-dist',
-    'assets-to-dist',
-    'views',
-    'dev-styles',
-    'dev-server'
-  )
-);
+function devTasks() {
+  return series(
+    commonTasks(),
+    devStyles,
+    devServer
+  );
+}
 
 // ===============
 // PRODUCTION
 // ===============
 
 // Compile scss to css, add prefixes and minify.
-gulp.task('prod-styles', function() {
-  return gulp
-    .src(MAIN_STYLE)
+function prodStyles() {
+  return src(files.styles.main)
     .pipe(sass().on('error', sass.logError))
     .pipe(
       postcss([
-        tailwindcss(TW_STYLE),
+        tailwindcss(files.styles.tw),
         autoprefixer(),
         cssnano({ preset: 'default' })
       ])
     )
-    .pipe(gulp.dest(DIST_STYLES));
-});
-
-// Remove unused css
-gulp.task('prod-purge-styles', function() {
-  return gulp
-    .src(`${DIST_STYLES}/*.css`)
-    .pipe(
-      purgecss({
-        content: [`${DIST}/**/*.html`],
-        extractors: [
-          {
-            extractor: purgeHtml,
-            extensions: ['html']
-          }
-        ]
-      })
-    )
-    .pipe(gulp.dest(DIST_STYLES));
-});
+    .pipe(dest(files.styles.dist));
+}
 
 // Run production tasks
-gulp.task(
-  'prod',
-  gulp.series('clean-dist', 'assets-to-dist', 'views', 'prod-styles')
-);
+function prodTasks() {
+  return series(commonTasks(), prodStyles);
+}
+
+// ====================
+// Public tasks
+// ====================
+exports.devTasks = devTasks();
+exports.prodTasks = prodTasks();
